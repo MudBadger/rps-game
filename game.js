acd5f8f4ae6958939d69c950a280da6ff0a9031a
Gaëtan Charolle <gaetan.charolle@gmail.com>
2022-06-01 15:17:32 +0200

    Ajout alert du résultat du jeu et petites corrections

--- game.js
+++ game.js
@@ -7,17 +7,20 @@ function computerPlay(arr){
 function playRound(playerSelection, computerSelection){
     if((playerSelection === moveArray[0] && computerSelection === moveArray[2]) || (playerSelection === moveArray[1] && computerSelection === moveArray[0]) || (playerSelection === moveArray[2] && computerSelection === moveArray[1])){
         console.log('You win');
+        alert("player - " + playerSelection + "\ncomputer - " + computerSelection + "\nYOU WIN!")
     } else if(playerSelection === computerSelection){
         console.log("It's a tie!");
+        alert("player - " + playerSelection + "\ncomputer - " + computerSelection + "\nIT'S A TIE!")
+
     } else {
         console.log("You lose!");
+        alert("player - " + playerSelection + "\ncomputer - " + computerSelection + "\nYOU LOSE!")
     }
 }
 const moveArray = ['rock', 'paper', 'scissors'];
-const result = computerPlay(moveArray);
 
 const playerSelection = prompt('Rock, paper or scissors? :');
-const computerSelection = result;
+const computerSelection = computerPlay(moveArray);
 console.log(playRound(playerSelection, computerSelection));
 console.log("player : " +playerSelection);
-console.log("computer : "+result);
+console.log("computer : " +computerSelection);
