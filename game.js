d7f968cfcca65b0a8af54725ab005f3e6c22b719
Gaëtan Charolle <gaetan.charolle@gmail.com>
2022-06-01 14:49:59 +0200

    Ajout fonction playRound

--- game.js
+++ game.js
@@ -4,6 +4,20 @@ function computerPlay(arr){
     return move;
 }
 
-const moveArray = ['Rock', 'Paper', 'Scissors'];
+function playRound(playerSelection, computerSelection){
+    if((playerSelection === moveArray[0] && computerSelection === moveArray[2]) || (playerSelection === moveArray[1] && computerSelection === moveArray[0]) || (playerSelection === moveArray[2] && computerSelection === moveArray[1])){
+        console.log('You win');
+    } else if(playerSelection === computerSelection){
+        console.log("It's a tie!");
+    } else {
+        console.log("You lose!");
+    }
+}
+const moveArray = ['rock', 'paper', 'scissors'];
 const result = computerPlay(moveArray);
-console.log(result);
+
+const playerSelection = prompt('Rock, paper or scissors? :');
+const computerSelection = result;
+console.log(playRound(playerSelection, computerSelection));
+console.log("player : " +playerSelection);
+console.log("computer : "+result);
