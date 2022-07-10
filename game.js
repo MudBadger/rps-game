0547c35b0c68c93757a82b52082d08449d711fce
Gaëtan Charolle <gaetan.charolle@gmail.com>
2022-07-10 17:05:13 +0200

    correction mineure du code

--- game.js
+++ game.js
@@ -8,7 +8,9 @@ function computerPlay(arr){
 
 function playRound(playerSelection, computerSelection){
 
-    if((playerSelection === moveArray[0] && computerSelection === moveArray[2]) || (playerSelection === moveArray[1] && computerSelection === moveArray[0]) || (playerSelection === moveArray[2] && computerSelection === moveArray[1])){
+    if((playerSelection === moveArray[0] && computerSelection === moveArray[2])
+             || (playerSelection === moveArray[1] && computerSelection === moveArray[0])
+              || (playerSelection === moveArray[2] && computerSelection === moveArray[1])){
         alert("You win! " + playerSelection + " beats " + computerSelection);
         youWin++;
     } else if(playerSelection === computerSelection){
@@ -22,7 +24,7 @@ function playRound(playerSelection, computerSelection){
 }
 
 function winner(){
-    if(youWin < tie && computerWin < tie || youWin === computerWin){
+    if((youWin < tie && computerWin < tie) || youWin === computerWin){
         alert('It\'s a Tie! Player :' + youWin + ' Computer :' + computerWin + ' Tie :' + tie);
     } else if (youWin > computerWin){
         alert('You win! Player :' + youWin + ' Computer :' + computerWin + ' Tie :' + tie);
