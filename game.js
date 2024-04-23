326ebf85f7aac38ae6681a2aef72e60fe2539ed5
Gaëtan Charolle <gaetan.charolle@gmail.com>
2024-04-23 17:38:16 +0200

    ajout event listener pour jouer avec boutons

--- game.js
+++ game.js
@@ -30,17 +30,40 @@ function winner() {
 }
 
 function game() {
-    for (let i = 0; i <= 5; i++) {
-        const playerChoice = prompt("Rock, paper or scissors? :").toLowerCase();
-        const computerChoice =
-            choicesArray[Math.floor(Math.random() * choicesArray.length)];
-        console.log(playRound(playerChoice, computerChoice));
-    }
-    console.log(winner());
+    if (!playerChoice) return;
+    const computerChoice =
+        choicesArray[Math.floor(Math.random() * choicesArray.length)];
+    console.log(playRound(playerChoice, computerChoice));
 }
+
+const choiceContainer = document.querySelector("#choice-container");
+
+choiceContainer.addEventListener("click", (event) => {
+    let target = event.target;
+    switch (target.id) {
+        case "scissors":
+            playerChoice = "scissors";
+            break;
+        case "rock":
+            playerChoice = "rock";
+            break;
+        case "paper":
+            playerChoice = "paper";
+            break;
+    }
+    choiceArray.push(playerChoice);
+    console.log(choiceArray.length);
+    if (choiceArray.length === 6) {
+        console.log(winner());
+    } else {
+        console.log(game());
+    }
+});
 
 let youWin = 0,
     tie = 0,
     computerWin = 0;
+let playerChoice = "";
+let choiceArray = [];
+
 const choicesArray = ["rock", "paper", "scissors"];
-console.log(game());
